f4ede89c4ae3827e6ebc157129fbf120ae74acd6
TKPareKongma <thitapakongma5@gmail.com>
2024-06-02 20:21:06 +0700

    22

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function App() {
+
+  const [todoList, setTodoList] = useState(["task 1"])
+  const [text, setText] = useState("")
+
+  const onTextChange = ({target: {value}}) =>{
+
+      console.log(value);
+
+
+
+      setText(value)
+  }
+  
+  const AddTask =() => {
+
+    setTodoList([
+      ...todoList,
+      text
+
+
+
+    ])
+
+  }
+
 
   return (
     <div>
 
-      <input type="text" />
-      <button>Add task</button>
+      <input type="text" value={text} onChange={ onTextChange }/>
+      <button onClick={AddTask}>Add task</button>
+    
 
       <div className="todo">
-        task 1
+        { 2 + 1 }
       </div>
 
       <div className="todo">
         task 2
       </div>
-      
+
     </div>
   )
 }
